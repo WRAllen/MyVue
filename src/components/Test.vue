3aad141aed4fa9c01b6ad11099fbe649aba25149
WRAllen <1072274105@qq.com>
2020-03-27 22:41:43 +0800

    修改首页请求新闻数据

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -5,26 +5,6 @@
         <img src="../assets/logo.png" width="100" />
       </b-col>
     </b-row>
-
-    <b-card-group deck class="mt-3">
-      <b-card>
-        <h1>Login</h1>
-        <p class="text-muted">Sign in to your account</p>
-        <login-box />
-      </b-card>
-
-      <b-card class="text-white bg-primary">
-        <b-card-text>
-          <h2>Sign up</h2>
-          <br />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
-          </p>
-          <b-link :to="{ path: '/register' }" class="btn btn-primary active mt-1">Register Now!</b-link>
-        </b-card-text>
-      </b-card>
-    </b-card-group>
   </b-container>
 </template>
 
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,9 +1,31 @@
 <template>
   <b-container>
     <h1>Welcome</h1>
+    <ul>
+      <li v-for="(data, index) in news" :key="index">
+        <a :href="data.Url" target="_blank">{{data.Title}}</a>
+      </li>
+    </ul>
   </b-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      page: 1,
+      news: []
+    };
+  },
+  created() {
+    this.axios
+      .get(this.$store.state.BASEURL + "/new", {
+        params: { Page: this.page }
+      })
+      .then(response => {
+        this.news = this.news.concat(response.data);
+      });
+    this.page += 1;
+  }
+};
 </script>
